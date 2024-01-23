112bf44c86172599532a40e58547d2761c5c6fe8
aryanofficialjain <aryanofficialjain@gmail.com>
2024-01-23 11:42:37 +0530

    Just completed everthih meta data also write

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -2,7 +2,6 @@ import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
 
-
 export function CreateInvoice({id} : {id : string}) {
   return (
     <Link
@@ -18,7 +17,7 @@ export function CreateInvoice({id} : {id : string}) {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+    href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
@@ -26,9 +25,9 @@ export function UpdateInvoice({ id }: { id: string }) {
   );
 }
 
-
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+ 
   return (
     <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
@@ -37,4 +36,5 @@ export function DeleteInvoice({ id }: { id: string }) {
       </button>
     </form>
   );
-  }
+}
+
